8da17eb38c7975491490b6654314b4db50a75be6
Erin Wise <erinannewise@gmail.com>
2016-10-25 12:48:15 -0400

    initial commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,5 @@
+const ohHey = "Hello World";
+
+const numbers = [1,2,3];
+
+console.log(ohHey);
---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+var numbers = [1, 2, 3];
+
+console.log(ohHey);
